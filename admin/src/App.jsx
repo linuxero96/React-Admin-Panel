b6587da4116339cb92ebb820274a964cc8bf6462
linuxero96 <al-felipe@hotmail.com>
2023-01-31 14:41:15 -0500

    create react with vite

---
+++ admin/src/App.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App () {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      Andres Diaz
+    </div>
+  )
+}
+
+export default App
