26e7b93e9b5e69beca059f922e3e919037c62dcc
linuxero96 <al-felipe@hotmail.com>
2023-02-01 12:50:01 -0500

    add routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,8 +1,32 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { Home } from './pages/home/Home'
+import Login from './pages/login/Login'
+import Single from "./pages/single/Single"
+import New from "./pages/new/New"
+import List from './pages/list/List'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 function App () {
   return (
-    // eslint-disable-next-line react/react-in-jsx-scope
     <div className="App">
-      Andres Diaz
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path=":new" element={<New />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path=":new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
