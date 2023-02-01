a98cc9107ca502731757cebef1310309575d37fe
linuxero96 <al-felipe@hotmail.com>
2023-02-01 17:31:34 -0500

    add widgets container

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
+import Navbar from '../../assets/components/navbar/Navbar'
 import Sidebar from '../../assets/components/sidebar/Sidebar'
+import Widget from '../../assets/components/widget/Widget'
+import Featured from '../../assets/components/featured/Featured'
+import Chart from '../../assets/components/chart/Chart'
 
 export const Home = () => {
   return (
     <>
-    <div className="flex">
+    <div className='flex'>
     <Sidebar />
-      <div className="bg-blue-700 flex-1">
-        Container
+      <div className='flex-1'>
+        <Navbar />
+        <div className='flex p-5 gap-5'>
+          <Widget type="user"/>
+          <Widget type="order"/>
+          <Widget type="earning"/>
+          <Widget type="balance"/>
+          <Widget type=""/>
+        </div>
+        <div>
+          <Featured />
+          <Chart />
+        </div>
       </div>
     </div>
     </>
